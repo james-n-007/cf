cdb36011bd0993ff27978ca7d2a5386f2b34951c
CoolDUE69 <65655058+CoolDUE69@users.noreply.github.com>
2020-06-04 15:19:20 +0530

    Update 202a.cxx

--- 202a.cxx
+++ 202a.cxx
@@ -1,23 +1,70 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-       string s;
-       cin>>s;
-       sort(s.begin(),s.end()) ;
-       int n = s.length();
-       int count = 0 ;
-       char check = s[n-1] ;
-       for (int i = 0; i < n; ++i)
-       {
-              if (s[i] == check)
-              {
-                     /* code */
-                     count ++ ;
-              }
-              /* code */
-       }
-    while(count>0){
-        cout<< check ;
-        count -- ;
+
+#define sf scanf
+#define pf printf
+#define ssf sscanf
+#define spf sprintf
+#define fsf fscanf
+#define fpf fprintf
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define scase sf ("%d",&tc)
+#define sn sf ("%d",&n)
+#define whilecase while (tc--)
+#define eof while (cin >> n)
+#define forloop for (pos=1; pos<=tc; pos++)
+#define arrayloop (i=0; i<n; i++)
+#define cinstr cin >> str
+#define getstr getline (cin,str)
+#define pcase pf ("Case %d: ",pos)
+#define vi vector <int>
+#define pii pair <int,int>
+#define mii map <int,int>
+#define pb push_back
+#define in insert
+#define llu unsigned long long
+#define lld long long
+#define U unsigned int
+#define endl "\n"
+
+const int MOD = 1000000007;
+const int MAX = 1000005;
+
+int SetBit (int n, int x) { return n | (1 << x); }
+int ClearBit (int n, int x) { return n & ~(1 << x); }
+int ToggleBit (int n, int x) { return n ^ (1 << x); }
+bool CheckBit (int n, int x) { return (bool)(n & (1 << x)); }
+
+char str[15];
+
+int main (void)
+{
+    /*
+    	freopen ("input.txt","r",stdin);
+    	freopen ("output.txt","w",stdout);
+    */
+
+    int len,i,n;
+    char x;
+
+    while (sf ("%s",str) != EOF)
+    {
+        len = strlen(str);
+        map <char,int> mp;
+
+        for (i=0; str[i]; i++)
+            mp[str[i]]++;
+
+        map <char,int> :: reverse_iterator r = mp.rbegin();
+
+        x = r->first;
+        n = r->second;
+
+        for (i=0; i<n; i++)
+            pf ("%c",x);
+
+        pf ("\n");
     }
+
+    return 0;
 }
