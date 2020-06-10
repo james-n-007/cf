75f399c223f174d897d26a3896f4551858b15c57
james-n-007 <58572502+james-n-007@users.noreply.github.com>
2020-06-10 23:52:54 +0530

    Create AE_threesum.cxx

---
+++ AE_threesum.cxx
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+       int arr[] = {1,3,4,5,6,-7,-2} ;
+       sort(arr,arr+7) ;
+       cout<<arr[6]<<arr[5]<<arr[4];
+       return 0 ;
+
+}
